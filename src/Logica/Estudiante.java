7d636fa7b0ec58984c3aa737acec7c3aa5330ca8
Fakundito <facuman030@gmail.com>
2019-10-08 11:23:16 -0300

    Cuarto Commit - Facu

--- src/Logica/Estudiante.java
+++ src/Logica/Estudiante.java
@@ -7,10 +7,28 @@ enum Orientacion1{
 public class Estudiante extends Usuario{
 
 	private Orientacion orient;
+	private int tope;
 	
-	public Estudiante(int id, int CI, String nombre, String apellido, String mail, String password, TipoUsuario typeUser, Orientacion orient) {
+	public Estudiante(int id, int CI, String nombre, String apellido, String mail, String password, TipoUsuario typeUser, Orientacion orient, int tope) {
 		super(id, CI, nombre, apellido, mail, password, typeUser, orient);
-		this.orient=orient;
+		this.setOrient(orient);
+		this.setTope(tope);
+	}
+
+	public int getTope() {
+		return tope;
+	}
+
+	public void setTope(int tope) {
+		this.tope = tope;
+	}
+
+	public Orientacion getOrient() {
+		return orient;
+	}
+
+	public void setOrient(Orientacion orient) {
+		this.orient = orient;
 	}
 
 }
